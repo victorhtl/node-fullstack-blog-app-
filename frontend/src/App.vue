82381063c894eca44fc4ab648922d5e9d5e6e331
victorhtl <vht.luna@gmail.com>
2024-10-01 10:13:16 -0300

    frontend aula 1

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,41 @@
 <template>
 	<div id="app">
-		<h1>Versão Inicial</h1>
+		<Header />
+		<Menu />
+		<Content />
+		<Footer />
 	</div>
 </template>
 
 <script>
+import Header from "./components/template/Header.vue"
+import Content from "./components/template/Content.vue"
+import Menu from "./components/template/Menu.vue"
+import Footer from "./components/template/Footer.vue"
 export default {
 	name: "App",
+	components: {Header, Content, Menu, Footer}
 }
 </script>
 
 <style>
+	*{
+		font-family: "Lato", sans-serif;
+	}
+	body {
+		margin: 0;
+	}
+	#app{
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-smoothing: grayscale;
 
+		height: 100vh;
+		display: grid;
+		grid-template-rows: 60px 1fr 40px;
+		grid-template-columns: 300px 1fr;
+		grid-template-areas:
+			"header header"
+			"menu content"
+			"menu footer"
+	}
 </style>
