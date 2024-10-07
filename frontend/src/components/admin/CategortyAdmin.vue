fb2c4d37c6a26f03f6022ef6cb7b079387babcf6
victorhtl <vht.luna@gmail.com>
2024-10-07 14:43:23 -0300

    categories table

--- frontend/src/components/admin/CategortyAdmin.vue
+++ frontend/src/components/admin/CategortyAdmin.vue
@@ -1,12 +1,40 @@
 <template>
     <div class="category-admin">
-        <h1>Category Component</h1>
+        <b-form>
+
+        </b-form>
+        <b-table striped hover :items="categories" :fields="fields">
+            
+        </b-table>
     </div>
 </template>
 
 <script>
+import axios from 'axios';
+import { baseApiUrl } from '../../../global'
 export default {
-    name: 'CategoryAdmin'
+    name: 'CategoryAdmin',
+    data: function(){
+        return {
+            categories: [],
+            fields: [
+                {key: 'id', label: 'Code', sortable: true},
+                {key: 'name', label: 'Name', sortable: true},
+                {key: 'parentId', label: 'Parent', sortable: true},
+                {key: 'path', label: 'Path', sortable: true},
+                {key: 'actions', label: 'Actions'}
+            ]
+        }
+    },
+    methods: {
+        loadCategories(){
+            const url = `${baseApiUrl}/categories`
+            axios.get(url).then(res => this.categories = res.data)
+        }
+    },
+    mounted(){
+        this.loadCategories()
+    }
 }
 </script>
 
